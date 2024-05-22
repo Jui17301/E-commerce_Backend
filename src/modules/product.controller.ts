52f0f682316c131596048b1232004420c69f2d39
Jui <cse_1912020087@gmail.com>
2024-05-22 13:04:17 +0600

    post data is updated

--- src/modules/product.controller.ts
+++ src/modules/product.controller.ts
@@ -4,14 +4,20 @@ import { ProductServices } from "./product.service";
 
 
 const createProduct = async(res:Response,req:Request)=>{
- 
+ try {
+
   const productData = req.body;
   const result = await ProductServices.createProduct(productData)
     res.json({
       success:true,
-      message:'Product is created successfully',
+      message:'Product created successfully!',
       data:result,
     })
+  
+ } catch (error) {
+  console.log(error)
+ }
+ 
   }
 
   export const ProductControllers ={
